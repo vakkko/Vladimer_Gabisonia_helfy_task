6707daf20bd5f6434ce61ba035b132168f2b045f
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-03-04 17:31:58 +0400

    feat: configure cors and send the post request

--- task-manager/frontend/src/components/TaskForm/TaskForm.tsx
+++ task-manager/frontend/src/components/TaskForm/TaskForm.tsx
@@ -9,8 +9,51 @@ const TaskForm: React.FC = () => {
   const description = useHandleChange();
   const priority = useHandleChange();
 
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  const createdAt = `${year}-${month}-${day}`;
+
+  const URL = "http://localhost:4000/api/tasks";
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let titleValue = title.value;
+    let descriptionValue = description.value;
+    let priorityValue = priority.value;
+
+    const postData = async () => {
+      try {
+        const response = await fetch(URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: titleValue,
+            description: descriptionValue,
+            priority: priorityValue,
+            createdAt,
+            completed: false,
+          }),
+        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        titleValue = "";
+        descriptionValue = "";
+        priorityValue = "";
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    postData();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title">Title</label>
       <br />
       <input
